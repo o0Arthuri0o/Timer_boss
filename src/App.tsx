cac1084f4e9c1e14c36f96af3d27da4d5aad6c0d
B2441_KhussainovAA <nevisipaus@gmail.com>
2024-02-09 20:16:59 +0300

    Инициализация проекта

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Timer</h1>
+    </>
+  )
+}
+
+export default App
